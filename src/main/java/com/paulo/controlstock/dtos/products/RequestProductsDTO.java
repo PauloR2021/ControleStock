b9c9a2af295c9aa0c6a8b5dde7ee4383ef8a94b4
PauloR2021 <paulotrindadeoficial@gmail.com>
2026-03-30 22:24:41 -0300

    Adicionando o DTO de Request de produtos

--- src/main/java/com/paulo/controlstock/dtos/products/RequestProductsDTO.java
+++ src/main/java/com/paulo/controlstock/dtos/products/RequestProductsDTO.java
@@ -1,4 +1,30 @@
 package com.paulo.controlstock.dtos.products;
 
-public record RequestProductsDTO() {
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+
+public record RequestProductsDTO(
+
+        @NotBlank(message = "Nome não pode estar em branco")
+        String name,
+
+        @NotBlank(message = "Descrição não pode estar em branco")
+        String description,
+
+        String category,
+
+        @NotNull(message = "O preço de compra é obrigatório")
+        @Positive(message = "O preço de compra deve ser maior que Zero")
+        Double price,
+
+        @Positive(message = "O preço de vendas deve ser maior que Zero")
+        Double venda,
+
+        @NotNull(message = "O estoque é obrigatório")
+        @PositiveOrZero(message = "O estoque não pode ser negativo")
+        Integer stock
+) {
 }
