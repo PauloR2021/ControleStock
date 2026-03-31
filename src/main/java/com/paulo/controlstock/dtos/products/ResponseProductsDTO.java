ea51180cba0f117aaa9858f9a85cff18eef589d4
PauloR2021 <paulotrindadeoficial@gmail.com>
2026-03-30 22:25:17 -0300

    Criando a o DTO para o response dos produtos

--- src/main/java/com/paulo/controlstock/dtos/products/ResponseProductsDTO.java
+++ src/main/java/com/paulo/controlstock/dtos/products/ResponseProductsDTO.java
@@ -1,4 +1,12 @@
 package com.paulo.controlstock.dtos.products;
 
-public record ResponseProductsDTO() {
+public record ResponseProductsDTO(
+        Integer id,
+        String name,
+        String description,
+        String category,
+        Double price,
+        Double venda,
+        Integer stock
+) {
 }
