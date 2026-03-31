6d5d651a8982ddc7acffff73b54b97fb9de4d7bb
PauloR2021 <paulotrindadeoficial@gmail.com>
2026-03-30 22:26:08 -0300

    Update na clase de exeption

--- src/main/java/com/paulo/controlstock/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/paulo/controlstock/exceptions/GlobalExceptionHandler.java
@@ -3,10 +3,12 @@ package com.paulo.controlstock.exceptions;
 import com.paulo.controlstock.api.ResponseApiControl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -16,5 +18,20 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(new ResponseApiControl<>(false,ex.getMessage(), null,HttpStatus.NOT_FOUND.value()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponseApiControl<Void>> methodArgumentNotValidException(MethodArgumentNotValidException ex) {
+
+        return ResponseEntity
+                .status(HttpStatus.METHOD_NOT_ALLOWED)
+                .body(new ResponseApiControl<>(false,ex.getMessage(),null,HttpStatus.METHOD_NOT_ALLOWED.value()));
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ResponseApiControl<Void>> runtimeException(RuntimeException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new ResponseApiControl<>(false,ex.getMessage(),null,HttpStatus.NOT_FOUND.value()));
     }
 }
