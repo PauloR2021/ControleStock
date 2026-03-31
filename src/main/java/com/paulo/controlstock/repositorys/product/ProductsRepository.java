d00b3193fb621af270b34f2592e15e316f6e9993
PauloR2021 <paulotrindadeoficial@gmail.com>
2026-03-30 22:26:38 -0300

    Adicionando a classe de repositorio do produto

--- src/main/java/com/paulo/controlstock/repositorys/product/ProductsRepository.java
+++ src/main/java/com/paulo/controlstock/repositorys/product/ProductsRepository.java
@@ -3,7 +3,11 @@ package com.paulo.controlstock.repositorys.product;
 import com.paulo.controlstock.models.product.ProductsModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductsRepository extends JpaRepository<ProductsModel, Integer> {
+
+    List<ProductsModel> findByNameContainingIgnoreCaseOrDescriptionContainingIgnoreCase(String name, String description);
 
 
 }
