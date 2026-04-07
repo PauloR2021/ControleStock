80daec7ee291d51f6e896ad8c56b754cf91d72e0
PauloR2021 <paulotrindadeoficial@gmail.com>
2026-04-07 18:55:51 -0300

    Ajustando o userData

--- app/src/main/java/com/prsoftware/appcontrolstock/dto/user/UserData.java
+++ app/src/main/java/com/prsoftware/appcontrolstock/dto/user/UserData.java
@@ -1,10 +1,19 @@
 package com.prsoftware.appcontrolstock.dto.user;
 
+import com.google.gson.annotations.SerializedName;
+
+import kotlinx.serialization.Serializable;
+
 //Classe para Salvar as informações do Usuário
 public class UserData {
+    @SerializedName("id")
     private Integer id;
+
+    @SerializedName("name")
     private String name;
+    @SerializedName("username")
     private String username;
+    @SerializedName("role")
     private String role;
 
     public Integer getId() {
